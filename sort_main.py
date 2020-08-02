6bf4d0321718943fc22545083d9447b590882737
egormm <egormm@gmail.com>
2020-08-02 23:50:33 +0300

    add main entrypoint

--- sort_main.py
+++ sort_main.py
@@ -24,23 +24,26 @@ array_sort = [
     quick_sort
 ]
 
+def main():
+    with open("output.csv", 'w') as fout:
+        writer = csv.writer(fout, delimiter=',', quoting=csv.QUOTE_MINIMAL)
+        writer.writerow(['algorithm', 'small arr', 'big int', 'sorted', 'resorted', 'big arr'])
+        with open('input.txt', 'r') as fin:
+            [n], *arrays = list(map(lambda x: list(map(int, x.strip('\n').split(' '))), fin.readlines()))
+        for func in array_sort:
+            time_arr = []
+            for i in range(n):
+                true_sorted_arr = sorted(arrays[i])
 
-with open("output.csv", 'w') as fout:
-    writer = csv.writer(fout, delimiter=',', quoting=csv.QUOTE_MINIMAL)
-    writer.writerow(['algorithm', 'small arr', 'big int', 'sorted', 'resorted', 'big arr'])
-    with open('input.txt', 'r') as fin:
-        [n], *arrays = list(map(lambda x: list(map(int, x.strip('\n').split(' '))), fin.readlines()))
-    for func in array_sort:
-        time_arr = []
-        for i in range(n):
-            true_sorted_arr = sorted(arrays[i])
+                start_time = timer()
+                sorted_arr = func(arrays[i][:])
+                delta_time = round(timer() - start_time, 10)
 
-            start_time = timer()
-            sorted_arr = func(arrays[i][:])
-            delta_time = round(timer() - start_time, 10)
+                assert sorted_arr == true_sorted_arr, "NOT SORTED, with {}, \n[{}]\n[{}]" \
+                    .format(func.__name__, ', '.join(map(str, sorted_arr)), ', '.join(map(str, true_sorted_arr)))
 
-            assert sorted_arr == true_sorted_arr, "NOT SORTED, with {}, \n[{}]\n[{}]" \
-                .format(func.__name__, ', '.join(map(str, sorted_arr)), ', '.join(map(str, true_sorted_arr)))
+                time_arr += [delta_time]
+            writer.writerow([func.__name__] + time_arr)
 
-            time_arr += [delta_time]
-        writer.writerow([func.__name__] + time_arr)
+if __name__ == '__main__':
+    main()
